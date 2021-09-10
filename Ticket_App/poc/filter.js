70378af06ef28a8e8760d2f866f7bf6389c4b16e
shivamsingh124 <shivamsingh12042018@gmail.com>
2021-09-10 10:47:48 +0530

    Folder Sort

--- Ticket_App/poc/filter.js
+++
@@ -1,9 +0,0 @@
-let values = [2,3,4,5,6,7,8,9];
-
-let evenValues = values.filter( function(value){
-    if(value%2 == 0){
-        return true;
-    }
-    return false;
-})
-console.log(evenValues);
